973c844421d6dca2a50cedfc0720c306a1ef2fe2
seokminGong <seokmin9296@atnsgroup.com>
2023-06-19 17:02:41 +0900

    Map Sample(Study) Product Controller 수정, pmkdemo html 수정, js 코드 추가

--- force-app/main/default/lwc/pmkdemo/pmkdemo.js
+++ force-app/main/default/lwc/pmkdemo/pmkdemo.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getProduct from '@salesforce/apex/ProductController.getProduct';
 
-export default class Pmkdemo extends LightningElement {}
+const COLS = [  {label: '제품코덴티파이', fieldName: 'Product_codentify__c'},
+                {label: '일련번호', fieldName: 'Product_code__c'},
+                {label: '상태구분', fieldName: 'Product_status__c'},
+                {label: 'SC', fieldName: 'SC__c'}
+            ];
+export default class Pmkdemo extends LightningElement {
+    draftValues = [];
+
+    @wire(getProduct)
+    products;
+
+    columns = COLS;
+
+    // async handleSave(event){
+    //     const updateFieids = event.detail.draftValues;
+
+    //     this.draftValues = [];
+
+    //     try{
+    //         await updateContacts({contactsForUpdate: updateFieids});
+    //         this.dispatchEvent(
+    //             new ShowToastEvent({
+    //                 title: 'Success',
+    //                 message: 'Contacts updated',
+    //                 variant: 'success'
+    //             })
+    //         );
+    //         await refreshApex(this.products);
+    //     }catch(error){
+    //         this.dispatchEvent(
+    //             new ShowToastEvent({
+    //                 title: 'Error while updating or refreshing records',
+    //                 message: error.body.message,
+    //                 variant: 'error'
+    //             })
+    //         );
+    //     }
+    // }
+}
